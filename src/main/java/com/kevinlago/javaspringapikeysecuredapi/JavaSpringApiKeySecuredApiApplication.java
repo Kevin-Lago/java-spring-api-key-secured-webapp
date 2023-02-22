f931e4629c50cf2257809ddd7cbc0aaa4ebc5b01
Kevin Lago <kevinthelago@gmail.com>
2023-02-22 13:06:34 -0500

    Initial Commit

---
+++ src/main/java/com/kevinlago/javaspringapikeysecuredapi/JavaSpringApiKeySecuredApiApplication.java
@@ -0,0 +1,13 @@
+package com.kevinlago.javaspringapikeysecuredapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringApiKeySecuredApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringApiKeySecuredApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevinlago/javaspringapikeysecuredapi/JavaSpringApiKeySecuredApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinlago.javaspringapikeysecuredapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringApiKeySecuredApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
